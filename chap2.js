e7acc38630c77d5ac62674be360dc07bebf40751
AlishiaNaqvi <96846213+AlishiaNaqvi@users.noreply.github.com>
2023-03-11 10:51:23 +0500

    Add files via upload

---
+++ chap2.js
@@ -0,0 +1,77 @@
+
+
+////////////////////QUESTION 1 //////////////////
+
+var username;
+
+
+
+
+
+
+
+
+
+////////////////////QUESTION 2 //////////////////
+
+var myName = "Alishia Batool";
+
+
+
+
+
+
+
+////////////////////QUESTION 3 //////////////////
+
+var message = "Hello world";
+alert(message);
+
+
+
+
+
+
+/////////////////////QUESTION 4 //////////////////
+
+
+var studentName = "Jhone Doe" ;
+var age = "15 years old" ;
+var course = "Certified Mobile Application Development" ;
+alert(studentName);
+alert(age);
+alert(course);
+
+
+/////////////////////QUESTION 5 //////////////////
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+alert(pizza);
+
+
+
+
+/////////////////////QUESTION 6 //////////////////
+var email = "alishianaqvi2@gmail.com"
+alert ("My email address is " + email);
+
+
+
+
+
+///////////////////// QUESTION 7 //////////////////
+var book = "A smarter way to learn Javascript."
+alert ("I am trying to learn from the book " + book);
+
+
+
+
+
+///////////////////// QUESTION 8 //////////////////
+document.writeln("Yeah! I can write HTML content through Javascript.")
+
+
+
+
+///////////////////// QUESTION 9 //////////////////
+var pic = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(pic);
